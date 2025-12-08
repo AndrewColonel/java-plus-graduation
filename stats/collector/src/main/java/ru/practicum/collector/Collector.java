33b7c2f43720f5aa042243ab4b09fe3f9dee46e4
AndrewColonel <andrewcolonel@yandex.ru>
2025-12-08 20:10:32 +0300

    ewm - stage-3 - collector - #2

--- stats/collector/src/main/java/ru/practicum/collector/Collector.java
+++ stats/collector/src/main/java/ru/practicum/collector/Collector.java
@@ -1,9 +1,11 @@
-package ru.practicum;
+package ru.practicum.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Collector {
     public static void main(String[] args) {
         SpringApplication.run(Collector.class, args);

cce7a4ec5f2e4129692479df1df07c1c7172b423
AndrewColonel <andrewcolonel@yandex.ru>
2025-12-16 18:39:57 +0300

    ewm - stage-3 - aggregator final optimized - #18

--- stats/aggregator/src/main/java/ru/practicum/aggregator/service/SimilarityServiceImpl.java
+++ stats/aggregator/src/main/java/ru/practicum/aggregator/service/SimilarityServiceImpl.java
@@ -40,8 +40,6 @@ public class SimilarityServiceImpl implements SimilarityService {
 
     @Override
     public List<EventSimilarityAvro> similarityProcessing(UserActionAvro userActionAvro) {
-
-
         Long eventId = userActionAvro.getEventId();
         Long userId = userActionAvro.getUserId();
         Double actionWeight = getActionWeight(userActionAvro.getActionType());
@@ -50,91 +48,75 @@ public class SimilarityServiceImpl implements SimilarityService {
         log.trace("|||-- вес дейсвия {} равен: {}", userActionAvro.getActionType(), actionWeight);
 
         // 1. Сбор матрицs максимальных весов действий пользователя c мероприятиями
-        // собираем матрицу весов действий пользователей c мероприятиями в виде отображения. Map<Event, Map<User, Weight>>
+        // Обновляем матрицу весов действий пользователей c мероприятиями в виде отображения. Map<Event, Map<User, Weight>>
         Map<Long, Double> userWeightMap = actionMatrix.computeIfAbsent(eventId, k -> new HashMap<>());
-        // userWeightMap.merge(userId, actionWeight, Double::max);
-        Double oldWeight = actionMatrix.get(eventId).getOrDefault(userId, 0.0);
-        Double newWeight = userWeightMap.merge(userId, actionWeight, (old, newW) -> {
-            Double result = Math.max(old, newW);
-            log.trace("|||-- <<<<MAX WEIGHT COMPUTED>>>>> такой пользователь {} УЖЕ взаимодейстовал с мероприятием {}",
-                    userId, eventId);
-            log.trace("|||-- СТАРЫЙ  вес действия: {}", old);
-            log.trace("|||-- НОВЫЙ максимальный вес действия: {}", result);
-            return result;
-        });
-        log.trace("|||-- ТЕКУШЕЕ состояние вектора события:  {} = {}:", eventId, userWeightMap);
-        log.trace("|||-- текущее состояние матрицы всех событий {}", actionMatrix);
+        Double oldWeight = userWeightMap.getOrDefault(userId, 0.0);
+        Double newWeight = Math.max(oldWeight, actionWeight);
 
-        // 2. Пользователь взаимодействоал с мерпориятием с изменением веса события
-        if (newWeight > oldWeight) {
-            log.trace("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
-            log.trace("|||-- СТАРЫЙ  вес действия: {}", oldWeight);
-            log.trace("|||-- НОВЫЙ  вес действия: {}", newWeight);
-            log.trace("|||-- НАДО ПЕРЕСЧИТЫВАТь ДЛЯ СОБЫТИЯ {}", eventId);
-
-            // если частная сумма для весоы события уже была расчитана
-            // посчитаем дельту между старым весом и обновлённым и увеличим на неё частную сумму:
-            double deltaWeight = newWeight - oldWeight;
-            commonWeightSumMap.computeIfPresent(eventId, (key, value) -> value + deltaWeight);
-
-            // для расчета косинусного схождения - нужно пересчитать частную сумму для весов события
-            commonWeightSumMap.computeIfAbsent(eventId, id ->
-                    actionMatrix.getOrDefault(id, Collections.emptyMap()).values().stream()
-                            .mapToDouble(Double::doubleValue)
-                            // паттерн используется для безопасной обработки случая, когда мапа отсутствует:
-                            .sum());
-
-            log.trace("|||-- ОБЩАЯ СУММА ВЕСОВ для мероприятия: {} =  {}", eventId,
-                    commonWeightSumMap.getOrDefault(eventId, 0.0));
-            log.trace("|||-- ОБЩАЯ СУММА ВЕСОВ для ВСЕХ мероприятий: {} ", commonWeightSumMap);
-
-            // сумма минимальных весов для каждой пары мероприятий.
-            List<EventSimilarityAvro> eventSimilarityAvroList = new ArrayList<>();
-            for (Map.Entry<Long, Map<Long, Double>> actionMatrixEntry : actionMatrix.entrySet()) {
-                // пропускаем равный самому себе ID мероприятия
-                if (actionMatrixEntry.getKey().equals(eventId)) continue;
-
-                //пропускаем событие с которым этот пользователь не взаимодейстовал
-                if (!actionMatrixEntry.getValue().containsKey(userId)) continue;
-
-                long first = Math.min(eventId, actionMatrixEntry.getKey());
-                long second = Math.max(eventId, actionMatrixEntry.getKey());
-
-                // Готовим числитель
-                double numerator = userWeightMap.entrySet().stream().mapToDouble(entry ->
-                                Math.min(entry.getValue(),
-                                        actionMatrixEntry.getValue().getOrDefault(entry.getKey(), 0.0)))
-                        .sum();
-                log.trace("|||-- ЧИСЛИТЕЛЬ - ОБЩАЯ СУММА ВЕСОВ для {} и {} -> {}", first, second, numerator);
-
-                // готовим знаменатель
-                double denominator = Math.sqrt(commonWeightSumMap.getOrDefault(first, 0.0))
-                        * Math.sqrt(commonWeightSumMap.getOrDefault(second, 0.0));
-
-                log.trace("|||-- ЗНАМЕНАТЕЛЬ - ПРОИЗВЕДЕНИЕ КВАДРАТНЫХ КОРНЕЙ ОБЩИХ СУММ ВЕСОВ {} {} -> {}", first, second, denominator);
-
-                double cosineSimilarity = (denominator == 0.0) ? 0.0 : numerator / denominator;
-                log.trace("|||-- КОСИНУСНОЕ СХОЖДЕНИЕ -> {}", cosineSimilarity);
-
-                eventSimilarityAvroList.add(
-                        EventSimilarityAvro.newBuilder()
-                                .setEventA(first)
-                                .setEventB(second)
-                                .setScore(cosineSimilarity)
-                                .setTimestamp(Instant.now())
-                                .build());
-
-                log.trace("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
-                log.trace("|||-- СУММА МИНИМАЛЬНЫХ ВЕСОВ для мероприятий {} {} -> {}", first, second, numerator);
-            }
-            log.trace("|||-- Список AVRO -> {}", eventSimilarityAvroList);
-            return eventSimilarityAvroList;
-
+        // нет изменений — выходим
+        if (newWeight <= oldWeight) {
+            return List.of(); // нет изменений — выходим
         }
-        return List.of();
+
+        userWeightMap.put(userId, newWeight);
+        log.trace("|||-- МАКСИМАЛЬНЫЙ вес обновлён: {} -> {}", oldWeight, newWeight);
+
+        // посчитаем дельту между старым весом и обновлённым и увеличим на неё частную сумму:
+        // Обновляем общую сумму весов для события
+        double delta = newWeight - oldWeight;
+        commonWeightSumMap.merge(eventId, delta, Double::sum);
+
+        log.trace("|||-- Общая сумма весов для {}: {}", eventId, commonWeightSumMap.get(eventId));
+
+        List<EventSimilarityAvro> eventSimilarityAvroList = new ArrayList<>();
+
+        // делаем перебор, но только по событиям с userId
+        for (Map.Entry<Long, Map<Long, Double>> entry : actionMatrix.entrySet()) {
+            // пропускаем равный самому себе ID мероприятия
+            Long otherEventId = entry.getKey();
+            if (otherEventId.equals(eventId)) continue;
+
+            //пропускаем событие с которым этот пользователь не взаимодейстовал
+            Map<Long, Double> otherWeights = entry.getValue();
+            if (!otherWeights.containsKey(userId)) continue;
+
+            long first = Math.min(eventId, entry.getKey());
+            long second = Math.max(eventId, entry.getKey());
+
+            // Готовим числитель
+            double numerator = calculateNumerator(userWeightMap, otherWeights);
+            log.trace("|||-- ЧИСЛИТЕЛЬ - ОБЩАЯ СУММА ВЕСОВ для {} и {} -> {}", first, second, numerator);
+
+            // готовим знаменатель
+            double denominator = Math.sqrt(commonWeightSumMap.getOrDefault(eventId, 0.0))
+                    * Math.sqrt(commonWeightSumMap.getOrDefault(otherEventId, 0.0));
+            log.trace("|||-- ЗНАМЕНАТЕЛЬ - ПРОИЗВЕДЕНИЕ КВАДРАТНЫХ КОРНЕЙ ОБЩИХ СУММ ВЕСОВ {} {} -> {}", first, second, denominator);
+
+            double cosineSimilarity = (denominator == 0.0) ? 0.0 : numerator / denominator;
+            log.trace("|||-- КОСИНУСНОЕ СХОЖДЕНИЕ -> {}", cosineSimilarity);
+
+            eventSimilarityAvroList.add(
+                    EventSimilarityAvro.newBuilder()
+                            .setEventA(first)
+                            .setEventB(second)
+                            .setScore(cosineSimilarity)
+                            .setTimestamp(Instant.now())
+                            .build());
+            log.trace("|||-- СУММА МИНИМАЛЬНЫХ ВЕСОВ для мероприятий {} {} -> {}", first, second, numerator);
+        }
+        log.trace("|||-- Список AVRO -> {}", eventSimilarityAvroList);
+        return eventSimilarityAvroList;
     }
 
     // вспомогательные методы
+    private double calculateNumerator(Map<Long, Double> weightsA, Map<Long, Double> weightsB) {
+        return weightsA.entrySet().stream()
+                .mapToDouble(entry -> {
+                    Double weightB = weightsB.get(entry.getKey());
+                    return (weightB != null) ? Math.min(entry.getValue(), weightB) : 0.0;
+                })
+                .sum();
+    }
     private Double getActionWeight(ActionTypeAvro actionTypeAvro) {
         // опрееделим вес для каждого типа действий пользователея
         return switch (actionTypeAvro) {
